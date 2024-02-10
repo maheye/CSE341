f00ebe326c767d93a9045479e934a34a5ad81ef7
maheye <u277721157@byui.edu>
2024-02-11 00:24:16 +0800

    updates

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ const express = require('express');
 const app = express();
  
 app.get('/', (req, res) => {
-  res.send("Hello");
+  res.send("Hello!");
 });
- 
+
+app.get('/austin', (req, res) => {
+    res.send("Austin Ma");
+  });
+
 app.listen(process.env.PORT || 3000, () => {
   console.log('Web Server is listening at port ' + (process.env.PORT || 3000));
 });
